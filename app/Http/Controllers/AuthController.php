6e9996cf4815acfc8f8b4675d1d248734dd1d348
RisingRobocop <32514027+RisingRobocop@users.noreply.github.com>
2017-12-04 20:59:50 +0100

    auth
    metodi controller autenticazione e registrazione

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    //tranne il logout tutti i metodi possono esere chiamati solo se non si è loggati
+    public function __construct()
+    {
+       $this->middleware('guest')->expect('logout');
+    }
+
+    //mostra la form di registrazione per creare un utente
+    public function create()
+    {
+        //
+    }
+
+    //salva l'utente nel database
+    public function store(Request $request)
+    {
+        //
+    }
+
+    //mostra la form di login
+    public function Showlogin()
+    {
+
+    }
+
+    //prova ad autenticare
+    public function loginAttempt(Request $request)
+    {
+
+    }
+
+    //chiude la sessione
+    public function logout(Request $request)
+    {
+
+    }
+}
