1dc38bf6c9a2c0e1cfac7dc11e8c3801a6dda3eb
RisingRobocop <32514027+RisingRobocop@users.noreply.github.com>
2017-12-11 21:37:31 +0100

    front end

--- resources/views/admin/nations.blade.php
+++ resources/views/admin/nations.blade.php
@@ -4,41 +4,100 @@
     <title>administration</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- pureCSS -->
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+    <link rel="stylesheet" href="/css/layouts/pricing.css">
     <link rel="stylesheet" href="css/nations.css">
   </head>
   <body>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Administration</a>
+    <div class="pure-menu pure-menu-horizontal">
+        <a href="#" class="pure-menu-heading">ADMINISTRATION</a>
+        <ul class="pure-menu-list">
+            
+        </ul>
+    </div>
+
+    <div class="banner">
+        <h1 class="banner-head">
+            Nations
+        </h1>
+    </div>
+
+
+        <div class="center">
+          <p>
+            <h2>Drag and drop to sort the nations once you are done press complete. <br>
+            Double click a nation to edit it
+            </h2>
+
+          </p>
+          <a class="pure-button pure-button-primary" href="#">Complete</a>
+          <a class="pure-button pure-button-primary" href="#">Add nation</a>
         </div>
-      </div>
-    </nav>
 
-    <div class="container">
-    <h1 class='title'>Nations</h1>
 
-      <ul id='sortable' class='list-group'>
-        <li class="list-group-item">Antartica</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
+          <ul id="sortable">
+        <li class="1"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Cina"></li>
+        <li class="2"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="3"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default"><img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia"></li>
+        <li class="ui-state-default">
+          <div class="container">
+            <img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia">
+            <img src="uploads/svizzera_mappa.png" class='imgBackgroud'>
+            <div class="imgText">switzerland</div>
+          </div>
+        </li>
+        <li class="ui-state-default">
+          <div class="container">
+            <img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia">
+            <img src="uploads/svizzera_mappa.png" class='imgBackgroud'>
+            <div class="imgText">skyrim</div>
+          </div>
+        </li>
+        <li class="ui-state-default">
+          <div class="container">
+            <img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia">
+            <img src="uploads/svizzera_mappa.png" class='imgBackgroud'>
+            <div class="imgText">skyrim</div>
+          </div>
+        </li>
+        <li class="ui-state-default">
+          <div class="container">
+            <img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia">
+            <img src="uploads/svizzera_mappa.png" class='imgBackgroud'>
+            <div class="imgText">skyrim</div>
+          </div>
+        </li>
+        <li class="ui-state-default">
+          <div class="container">
+            <img src="uploads/russia_copertina.jpg" alt="Russia" title="Russia">
+            <img src="uploads/svizzera_mappa.png" class='imgBackgroud'>
+            <div class="imgText">skyrim</div>
+          </div>
+        </li>
       </ul>
 
-    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>
-      $('#sortable').sortable();
+      $('#sortable').sortable({
+        cursor: "move",
+          });
+    $( "li" ).dblclick(function() {
+      alert($(this).attr('class'));
+    });
 
     </script>
   </body>
