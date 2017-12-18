bea83400b1e99ac3ed93f837a65910699c942f32
RisingRobocop <32514027+RisingRobocop@users.noreply.github.com>
2017-12-18 11:48:10 +0100

    form modifica nazione

--- resources/views/admin/nation.blade.php
+++ resources/views/admin/nation.blade.php
@@ -1,9 +1,20 @@
 @extends('admin.layout.master')
 
 @section('content')
-  <div class="row">
-    <div class="col-md-12">
-
+  <form class="form-inline" method="POST" action='{{url('admin/nations/')}}'>
+    {{ csrf_field() }}
+    <div class="form-group">
+      <div class="text-muted">EN name</div>
+      <input type="text" class="form-control mx-sm-3" name="name_en">
     </div>
-  </div>
+    <div class="form-group">
+      <div class="text-muted">IT name</div>
+      <input type="text" class="form-control mx-sm-3" name="name_en">
+    </div>
+    <div class="form-group">
+      <div class="text-muted">DE name</div>
+      <input type="text" class="form-control mx-sm-3" name="name_en">
+    </div>
+
+  </form>
 @endsection
