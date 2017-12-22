e30fc9876625e7653e64a1ce8fba38b1bce249ae
RisingRobocop <32514027+RisingRobocop@users.noreply.github.com>
2017-12-22 16:11:50 +0100

    login più bello

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,33 @@
-@include('home.includes.head')
-  <div class="">
-    <form class="" action="login" method="post">
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Flat HTML5/CSS3 Login Form</title>
+
+
+
+      <link rel="stylesheet" href="{{ URL::asset('css/login.css')}}">
+
+
+</head>
+
+<body>
+
+  <div class="login-page">
+  <div class="form">
+    <form class="login-form" action="{{url('login')}}" method="post">
       {{ csrf_field() }}
-      <input type="text" name="user" value="">
-      <input type="password" name="password" value="">
-      <input type="submit" name="" value="login">
-      <input type="checkbox" name="remember" value="TRUE"> remember
+      <input type="text" name="user" placeholder="username"/>
+      <input type="password" name="password" placeholder="password"/>
+      <input type="submit" class="button" value="login">
     </form>
   </div>
+</div>
+
+
+
+
 </body>
+
 </html>
