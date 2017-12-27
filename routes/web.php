4e01199b391cc42c44eab1b9dada0cc61429dcf2
RisingRobocop <32514027+RisingRobocop@users.noreply.github.com>
2017-12-27 17:32:45 +0100

    :santa:

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 //Route::get('/register','AuthController@create');
 //Route::post('/register','AuthController@store');
 
-//URL::forceRootUrl('https://tokenNgrok');
+//URL::forceRootUrl('http://5bad0277.eu.ngrok.io');
 //gestione login
 Route::get('/login','AuthController@showlogin')->name('login');
 Route::post('/login','AuthController@login');
